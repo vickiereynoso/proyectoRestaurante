45a9b237ca344c9270dbe07fda95274f46e559ba
Victoria Denisse Reynoso <victoriareynoso88@gmail.com>
2023-08-02 16:19:48 -0300

    Creando proyecto

---
+++ proyectoRestaurante/src/main/java/com/proyectoRestaurante/proyectoRestaurante/App.java
@@ -0,0 +1,13 @@
+package com.proyectoRestaurante.proyectoRestaurante;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
